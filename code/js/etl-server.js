fafd8e3fd14a71f860256827842875cc111d1dc9
zepedros <zepedro4259@gmail.com>
2021-04-22 20:19:25 +0100

    started project layout

---
+++ code/js/etl-server.js
@@ -0,0 +1,28 @@
+'use strict'
+
+const DEFAULT_PORT = 8000;
+
+const PORT = process.argv[2] || DEFAULT_PORT;
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}))
+
+auth.initialize(app);
+
+const db = require('./etl-db');
+const data = require('./etl-data.js');
+
+const servicesCreator = require('./etl-services.js');
+const webApiCreator = require('./etl-web-api.js');
+
+const services = servicesCreator(data, db, auth);
+const webapi = webApiCreator(app, auth, services);
+
+app.use(express.static('../client/dist'));
+
+app.listen(PORT);
+console.log(`Listening at port ${PORT}\nAccess http://localhost:8000 to start`);
