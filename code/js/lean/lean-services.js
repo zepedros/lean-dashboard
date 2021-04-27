69404fb30a729899fa5acebc666cfbe0c0fea463
zepedros <a44805@alunos.isel.pt>
2021-04-27 19:07:25 +0100

    very small changes. now getting all issues put into elastic search

--- code/js/lean/lean-services.js
+++ code/js/lean/lean-services.js
@@ -1,11 +1,14 @@
 'use strict';
 
+const mock = require('./mock-db')
+
 function services(data, db){
 
     return {
 
         getIssues: async function () {
-            return db.getIssues()
+            //return db.getIssues()
+            return mock.getIssues()
         },
         getIssuesById: async function (id) {
             return db.getIssuesById(id)
