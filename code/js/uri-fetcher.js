e1621f3ab10c91c308c4751dd76f37811f955673
zepedros <zepedro4259@gmail.com>
2021-04-29 20:03:40 +0100

    uri fetcher now correctly throws errors

--- code/js/uri-fetcher.js
+++ code/js/uri-fetcher.js
@@ -1,44 +1,42 @@
 'use strict'
 const fetch = require('node-fetch')
-const defaultHeader = { 'Content-type': 'application/json'}
+const defaultHeader = {'Content-type': 'application/json'}
 const error = require("./error")
-const URI_METHOD = {GET: "get",POST:"post",PUT:"put",DELETE: "delete"}
+const URI_METHOD = {GET: "get", POST: "post", PUT: "put", DELETE: "delete"}
 
 module.exports = {
-    makeGetRequest : async function makeGetRequest(uri,headers, body, params) {
-      return makeRequest(uri, URI_METHOD.GET, body, headers || defaultHeader, params)
+    makeGetRequest: async function makeGetRequest(uri, headers, body, params) {
+        return makeRequest(uri, URI_METHOD.GET, body, headers || defaultHeader, params)
     },
 
-    makePostRequest:async function makePostRequest(uri, body, headers) {
+    makePostRequest: async function makePostRequest(uri, body, headers) {
         return makeRequest(uri, URI_METHOD.POST, body, headers || defaultHeader)
     },
 
-    makePutRequest: async function makePutRequest(uri,body,headers) {
+    makePutRequest: async function makePutRequest(uri, body, headers) {
         return makeRequest(uri, URI_METHOD.PUT, body, headers || defaultHeader)
     },
 
-    makeDeleteRequest: async function makeDeleteRequest(uri,body) {
+    makeDeleteRequest: async function makeDeleteRequest(uri, body) {
         return makeRequest(uri, URI_METHOD.DELETE, body)
     }
 }
 
 async function makeRequest(uri, method, body, headers, params) {
     console.log(`Making a request to ${uri} `)
-    try{
-      const response =  await fetch(uri,options(method,body,headers, params))
-      return await response.json();
+    const response = await fetch(uri, options(method, body, headers, params))
+    if (!response.ok ){
+        throw error.create(response.status, response.statusText)
     }
-    catch(err){
-      throw error.create(err.status || 400, err.reason ||'Something went wrong')
-    }
+    return await response.json();
 }
 
-function options(method,body, header, params) {
-        return {
-            method: method,
-            body: body?JSON.stringify(body): undefined,
-            headers: header,
-            params: params
+function options(method, body, header, params) {
+    return {
+        method: method,
+        body: body ? JSON.stringify(body) : undefined,
+        headers: header,
+        params: params
 
-          }
     }
+}
