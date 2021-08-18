7d56ad59620f16b3399c59c8192d711126c7d873
Tomás Santos <tomassms@gmail.com>
2021-08-18 01:38:49 +0100

    add chapters to report

--- code/leandashboard-ui/src/App.js
+++ code/leandashboard-ui/src/App.js
@@ -1,4 +1,3 @@
-//import logo from './logo.svg';
 import { useEffect, useState } from 'react';
 import { BrowserRouter as Router, Redirect, Route, Switch } from "react-router-dom";
 import useFetch from 'use-http';
--- code/leandashboard-ui/src/components/Projects/ProjectsTable.js
+++ code/leandashboard-ui/src/components/Projects/ProjectsTable.js
@@ -99,6 +99,7 @@ EnhancedTableHead.propTypes = {
   classes: PropTypes.object.isRequired,
   numSelected: PropTypes.number.isRequired,
   onRequestSort: PropTypes.func.isRequired,
+  //onSelectAllClick: PropTypes.func.isRequired,
   order: PropTypes.oneOf(['asc', 'desc']).isRequired,
   orderBy: PropTypes.string.isRequired,
   rowCount: PropTypes.number.isRequired,
